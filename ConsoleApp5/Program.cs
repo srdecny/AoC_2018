92830160fe01ca3f3c2b69c8c0d25d4d8e81a0c1
Vojtěch Srdečný <vojtech.srdecny@gmail.com>
2018-12-04 09:07:35 +0100

    3A

--- ConsoleApp5/Program.cs
+++ ConsoleApp5/Program.cs
@@ -15,24 +15,13 @@ namespace ConsoleApp5
         {
             int[,] cloth = new int[ClothSize, ClothSize];
             char[] fromSeparators = new char[] { ',', ':' };
-            List<int> IDs = new List<int>();
+
             var file = new StreamReader(File.Open(@"C:\Users\Vojta\Documents\input.txt", FileMode.Open));
             while (!file.EndOfStream)
             {
                 string line = file.ReadLine();
                 string[] words = line.Split();
-                IDs.Add(Int32.Parse(words[0].Split('#')[1]));
-            }
-            file.Close();
 
-
-            var file2 = new StreamReader(File.Open(@"C:\Users\Vojta\Documents\input.txt", FileMode.Open));
-            while (!file2.EndOfStream)
-            {
-                string line = file2.ReadLine();
-                string[] words = line.Split();
-
-                int id = Int32.Parse(words[0].Split('#')[1]);
 
                 int fromY = Int32.Parse(words[2].Split(fromSeparators)[0]);
                 int fromX = Int32.Parse(words[2].Split(fromSeparators)[1]);
@@ -43,18 +32,13 @@ namespace ConsoleApp5
                 for (int x = fromX; x < fromX + sizeX; x++)
                 {
                     for (int y = fromY; y < fromY + sizeY; y++)
-                    {   
-                        if (cloth[x, y] != 0)
-                        {
-                            IDs.Remove(id);
-                            IDs.Remove(cloth[x, y]);
-                        }
-                        cloth[x, y] = id;
+                    {
+                        cloth[x, y]++;
                     }
                 }
 
             }
-            //PrintCloth(cloth);
+
             int result = 0;
             for (int x = 0; x < ClothSize; x++)
             {
@@ -68,7 +52,7 @@ namespace ConsoleApp5
 
         }
 
-        static void PrintCloth(int[,] cloth)
+        void PrintCloth(int[,] cloth)
         {
             for (int x = 0; x < ClothSize; x++)
             {
