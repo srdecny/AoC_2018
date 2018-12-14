0e574e87301021006f124d23c5471be62fd6981c
Vojtěch Srdečný <vojtech.srdecny@gmail.com>
2018-12-14 22:18:41 +0100

    14A

--- ConsoleApp5/Program.cs
+++ ConsoleApp5/Program.cs
@@ -15,9 +15,7 @@ namespace ConsoleApp5
 
         static void Main(string[] args)
         {
-            long limit = 59414;
-            long bufferSize = limit.ToString().Count();
-            long[] buffer = new long[bufferSize];
+            long limit = 360781;
 
             Dictionary<long, HashSet<long>> recipes = new Dictionary<long, HashSet<long>>();
             for (long i = 0; i < 10; i++) recipes.Add(i, new HashSet<long>());
@@ -25,7 +23,6 @@ namespace ConsoleApp5
             long firstElf = 0;
             long secondElf = 1;
             long recipeCount = 2;
-
 
             recipes[3].Add(0);
             recipes[7].Add(1);
@@ -37,45 +34,22 @@ namespace ConsoleApp5
                 foreach (char c in (firstScore + secondScore).ToString())
                 {
                     recipes[Int64.Parse(c.ToString())].Add(recipeCount);
-                    LeftShiftArray(buffer, 1);
-                    buffer[bufferSize - 1] = Int64.Parse(c.ToString());
                     recipeCount++;
 
-                    long bufferSum = 0;
-                    char[] numbersArray = limit.ToString().ToCharArray();
-                    if (numbersArray.Contains(c))
+                    if (recipeCount > limit && recipeCount <= limit + 10)
                     {
-                        for (long b = 0; b < bufferSize; b++)
-                        {
-                            bufferSum += buffer[b] * (long)Math.Pow(10, (bufferSize - b - 1));
-                        }
-                        if (bufferSum == limit)
-                        {
-                            Console.WriteLine(recipeCount - bufferSize);
-                            goto EXIT;
-                        }
+                        Console.Write(c);
                     }
                 }
 
                 firstElf = (firstElf + firstScore + 1 ) % recipeCount;
                 secondElf = (secondElf + secondScore + 1) % recipeCount;
-
             }
             EXIT:
             Console.WriteLine();
             Console.WriteLine("Finished . . .");
             Console.ReadLine();
 
-        }
-
-        // https://stackoverflow.com/a/38483135
-        public static void LeftShiftArray<T>(T[] arr, long shift)
-        {
-            shift = shift % arr.Length;
-            T[] buffer = new T[shift];
-            Array.Copy(arr, buffer, shift);
-            Array.Copy(arr, shift, arr, 0, arr.Length - shift);
-            Array.Copy(buffer, 0, arr, arr.Length - shift, shift);
         }
 
     }
