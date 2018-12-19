78feb3d999a3728e8bccdaa2db141738aef9c35e
Vojtěch Srdečný <vojtech.srdecny@gmail.com>
2018-12-19 17:23:51 +0100

    19B

--- ConsoleApp5/Program.cs
+++ ConsoleApp5/Program.cs
@@ -16,7 +16,7 @@ namespace ConsoleApp5
             string input = @"C:\Users\srdecny\Documents\input.txt";
             Registry registry = new Registry();
             registry.ParseInput(input);
-            registry.RunProgram();
+            registry.DecompiledProgram();
         }
 
     }
@@ -184,9 +184,15 @@ namespace ConsoleApp5
         {
             int instructionCount = Program.Values.Count();
             int instructionPointer = 0;
+            this[0] = 1;
 
             while (instructionPointer < instructionCount)
             {
+                
+
+                Console.Write($"{instructionPointer}:  ");
+                DumpRegistry();
+
                 this[instructionRegistry] = instructionPointer;
                 performOperation(Program[instructionPointer]);
                 instructionPointer = this[instructionRegistry];
@@ -208,7 +214,75 @@ namespace ConsoleApp5
                 Opcode = opcode;
                 Values = values;
             }
+
+            public override string ToString()
+            {
+               return $"Opcode: {Opcode.ToString()}, values: [{Values[0]}, {Values[1]},{Values[2]}]";
+            }
         }
+
+        private void DumpRegistry()
+        {
+            Console.WriteLine($"A: {A}, B: {B}, C: {C}, D: {D}, E: {E}, F: {F}");
+        }
+
+        public void DecompiledProgram()
+        {
+            long a = 0;
+            long c = 10551432;
+
+
+            for (long b = 1; b <= c; b++)
+            {
+                if (c % b == 0) a += c / b;
+            }
+
+            Console.WriteLine(a);
+            Console.ReadLine();
+
+        }
+
+        #region input
+        /*
+        #ip 5
+        addi 5 16 5
+        seti 1 8 3
+        seti 1 1 1
+        mulr 3 1 4
+        eqrr 4 2 4
+        addr 4 5 5
+        addi 5 1 5
+        addr 3 0 0
+        addi 1 1 1
+        gtrr 1 2 4
+        addr 5 4 5
+        seti 2 7 5
+        addi 3 1 3
+        gtrr 3 2 4
+        addr 4 5 5
+        seti 1 5 5
+        mulr 5 5 5
+        addi 2 2 2
+        mulr 2 2 2
+        mulr 5 2 2
+        muli 2 11 2
+        addi 4 8 4
+        mulr 4 5 4
+        addi 4 20 4
+        addr 2 4 2
+        addr 5 0 5
+        seti 0 4 5
+        setr 5 8 4
+        mulr 4 5 4
+        addr 5 4 4
+        mulr 5 4 4
+        muli 4 14 4
+        mulr 4 5 4
+        addr 2 4 2
+        seti 0 7 0
+        seti 0 9 5
+        */
+        #endregion
 
 
     }
