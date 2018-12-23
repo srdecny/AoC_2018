c28f258eb4e48a4b308d3f70a7e4405256dbf196
Vojtěch Srdečný <vojtech.srdecny@gmail.com>
2018-12-23 16:11:51 +0100

    23A

--- ConsoleApp5/Program.cs
+++ ConsoleApp5/Program.cs
@@ -14,29 +14,25 @@ namespace ConsoleApp5
 
         static void Main(string[] args)
         {
+            string input = @"C:\Users\Vojta\Documents\input.txt";
             Map map = new Map();
-            map.CalculateGeologicIndex();
-            map.CalculateErosionLevel();
-            map.CalculateRiskLevel();
-            //map.SumRiskLevel();
-            map.Dijkstra();
+            map.ParseInput(input);
+            map.FindStrongestNanobot();
 
-            // 795 low
-            // 1072 high
-            // 1068 for another person
-            // 1064 not
             Console.WriteLine("Finished...");
             Console.ReadLine();
+
+            // 949 high
         }
 
     }
 
     public struct Coords
     {
-        public int X { get; set; }
-        public int Y { get; set; }
-        public int Z { get; set; }
-        public Coords(int x, int y, int z = 0)
+        public long X { get; set; }
+        public long Y { get; set; }
+        public long Z { get; set; }
+        public Coords(long x, long y, long z)
         {
             X = x;
             Y = y;
@@ -44,268 +40,59 @@ namespace ConsoleApp5
         }
     }  
 
-    public class Map
+    public struct Nanobot
     {
-        static Coords MaximumSize = new Coords(200, 1100); // the total area searched
-
-        static int Depth = 5616;
-        //static int Depth = 510;
-        //static Coords Target = new Coords(10,10);
-        static Coords Target = new Coords(10,785);
-        long[,] GeologicIndex = new long[MaximumSize.X + 1, MaximumSize.Y + 1];
-        int[,] ErosionLevel = new int[MaximumSize.X + 1, MaximumSize.Y + 1];
-        int[,] RiskLevel = new int[MaximumSize.X + 1, MaximumSize.Y + 1];
-
-        public void CalculateGeologicIndex()
+        public Coords Coordinates { get; }
+        public long Range { get; }
+        
+        public Nanobot(Coords coords, long range)
         {
-            for (int x = 0; x <= MaximumSize.X; x++)
-            {
-                for (int y = 0; y <= MaximumSize.Y; y++)
-                {
+            Coordinates = coords;
+            Range = range;
+        }
+    }
 
-                    if (x == 0 && y == 0) GeologicIndex[x, y] = 0;
-                    else if (x == Target.X && y == Target.Y) GeologicIndex[x, y] = 0;
-                    else if (x == 0) GeologicIndex[x, y] = y * 48271;
-                    else if (y == 0) GeologicIndex[x, y] = x * 16807;
-                    else GeologicIndex[x, y] = GetErosionLevel(new Coords(x, y));
-                }
+    class Map
+    {
+        private List<Nanobot> Nanobots = new List<Nanobot>();
+        public void ParseInput(string input)
+        {
+            StreamReader reader = new StreamReader(input);
+            var positionDelimiters = "pos=<,>".ToCharArray();
+            var rangeDelimiters = " r=".ToCharArray();
+
+            while (!reader.EndOfStream)
+            {
+                var words = reader.ReadLine().Split(',');
+                var positionX = words[0].Split(positionDelimiters, StringSplitOptions.RemoveEmptyEntries).Select(x => Int64.Parse(x)).ToArray();
+                var positionY = words[1].Split(positionDelimiters, StringSplitOptions.RemoveEmptyEntries).Select(x => Int64.Parse(x)).ToArray();
+                var positionZ = words[2].Split(positionDelimiters, StringSplitOptions.RemoveEmptyEntries).Select(x => Int64.Parse(x)).ToArray();
+                var range = words[3].Split(rangeDelimiters, StringSplitOptions.RemoveEmptyEntries).Select(x => Int64.Parse(x)).ToArray();
+                Nanobots.Add(new Nanobot(new Coords(positionX[0], positionY[0], positionZ[0]), range[0]));
             }
         }
 
-        public void CalculateErosionLevel()
+        public void FindStrongestNanobot()
         {
-            for (int x = 0; x <= MaximumSize.X; x++)
+            int maxNanobotAmount = Int32.MinValue;
+
+            int nanobotAmount = 0;
+            Nanobot strongestNanobot = Nanobots.OrderByDescending(x => x.Range).First();
+            foreach (var otherNanobot in Nanobots)
             {
-                for (int y = 0; y <= MaximumSize.Y; y++)
-                {
-                    
-                    ErosionLevel[x, y] = (int)((GeologicIndex[x, y] + Depth) % 20183);
-                }
+                if (CalculateManhattanDistance(strongestNanobot.Coordinates, otherNanobot.Coordinates) <= strongestNanobot.Range) nanobotAmount++;
             }
+            if (nanobotAmount > maxNanobotAmount) maxNanobotAmount = nanobotAmount;
+            Console.WriteLine(maxNanobotAmount);
         }
 
-        public long GetErosionLevel(Coords coords)
+        private long CalculateManhattanDistance(Coords first, Coords second)
         {
-            long LeftErosionLevel = ((GeologicIndex[coords.X - 1, coords.Y] + Depth) % 20183);
-            long RightErosionLevel = ((GeologicIndex[coords.X, coords.Y - 1] + Depth) % 20183);
-            return LeftErosionLevel * RightErosionLevel;
-        }
-        public void CalculateRiskLevel()
-        {
-            for (int x = 0; x <= MaximumSize.X; x++)
-            {
-                for (int y = 0; y <= MaximumSize.Y; y++)
-                {
-                    RiskLevel[x, y] = ErosionLevel[x, y] % 3;
-                }
-            }
-
-        }
-        public void SumRiskLevel()
-        {
-            int level = 0;
-            for (int x = 0; x <= Target.X; x++)
-            {
-                for (int y = 0; y <= Target.Y; y++)
-                {
-                    level += RiskLevel[x, y];
-                }
-            }
-            Console.WriteLine(level);
-
-        }
-
-        public void Dijkstra()
-        {
-            // z coordinates == current equipment
-            Dictionary<Coords, List<Edge>> EdgeMap = new Dictionary<Coords, List<Edge>>();
-            Dictionary<Coords, int> shortestPath = new Dictionary<Coords, int>();
-            SimplePriorityQueue<Coords> searchQueue = new SimplePriorityQueue<Coords>();
-
-            for (int x = 0; x < MaximumSize.X; x++)
-            {
-                for (int y = 0; y < MaximumSize.Y; y++)
-                {
-                    for (int z = 0; z < 3; z++)
-                    {
-                        Coords currentCoords = new Coords(x, y, z);
-                        EdgeMap[currentCoords] = new List<Edge>();
-                        shortestPath.Add(currentCoords, int.MaxValue);
-                        int currentTerrain = RiskLevel[currentCoords.X, currentCoords.Y];
-
-                        // Add all changes of gear
-                        foreach (int equipment in Enumerable.Range(0, 3))
-                        {
-                            if (equipment != z)
-                            {
-                                switch (currentTerrain)
-                                {
-                                    // rocky, only torch and gear
-                                    case 0:
-                                        if (equipment == 0 || equipment == 1)
-                                        {
-                                            EdgeMap[currentCoords].Add(new Edge(7, new Coords(x, y, equipment)));
-                                        }
-                                        break;
-
-                                    // wet, only gear or neither
-                                    case 1:
-                                        if (equipment == 2 || equipment == 1)
-                                        {
-                                            EdgeMap[currentCoords].Add(new Edge(7, new Coords(x, y, equipment)));
-                                        }
-                                        break;
-                                    // narrow, only torch or neither
-                                    case 2:
-                                        if (equipment == 0 || equipment == 2)
-                                        {
-                                            EdgeMap[currentCoords].Add(new Edge(7, new Coords(x, y, equipment)));
-                                        }
-                                        break;
-                                }
-
-                            
-                            }
-                        }
-
-                        // Add all possible neighbours
-                        foreach (var neighbour in GetNeighbourCoords(currentCoords))
-                        {
-                            int neighbourTerrain = RiskLevel[neighbour.X, neighbour.Y];
-                            switch (currentCoords.Z)
-                            {
-                                // Torch equipped, can move to rocky or narrow
-                                case 0:
-                                    if (neighbourTerrain == 0 || neighbourTerrain == 2)
-                                    {
-                                        EdgeMap[currentCoords].Add(new Edge(1, neighbour));
-                                    }
-                                    break;
-                                // gear equipped, can move to wet or rocky
-                                case 1:
-                                    if (neighbourTerrain == 1 || neighbourTerrain == 0)
-                                    {
-                                        EdgeMap[currentCoords].Add(new Edge(1, neighbour));
-                                    }
-                                    break;
-                                // neither equipped, can move to wet or narrow
-                                case 2:
-                                    if (neighbourTerrain == 1 || neighbourTerrain == 2)
-                                    {
-                                        EdgeMap[currentCoords].Add(new Edge(1, neighbour));
-                                    }
-                                    break;
-                            }
-                        }
-
-                    
-                    }
-                }
-            }
-
-            searchQueue.Enqueue(new Coords(0, 0, 0), AStarHeuristics(new Coords(0,0,0), Target));
-            shortestPath[new Coords(0, 0, 0)] = 0;
-            HashSet<Coords> visitedCoords = new HashSet<Coords>();
-            HashSet<Coords> openCoords = new HashSet<Coords>();
-            openCoords.Add(new Coords(0, 0, 0));
-            while (searchQueue.Any())
-            {
-                var searchedCooords = searchQueue.Dequeue();
-                int currentTime = shortestPath[searchedCooords];
-
-                visitedCoords.Add(searchedCooords);
-                openCoords.Remove(searchedCooords);
-
-                if (searchedCooords.X == Target.X && searchedCooords.Y == Target.Y && searchedCooords.Z == 0)
-                {
-                    Console.WriteLine($"Target found, time elapsed: {currentTime}");
-                }
-
-                foreach (var edge in EdgeMap[searchedCooords])
-                {
-                    if (visitedCoords.Contains(edge.To))
-                    {
-                        continue;
-                    }
-
-                    int edgeDistance = edge.Value + currentTime;
-                    if (!openCoords.Contains(edge.To))
-                    {
-                        openCoords.Add(edge.To);
-                        searchQueue.Enqueue(edge.To, edgeDistance + AStarHeuristics(edge.To, Target));
-                    }
-                    else if (edgeDistance >= shortestPath[edge.To])
-                    {
-                        continue;
-                    }
-
-                    shortestPath[edge.To] = edgeDistance;
-                    searchQueue.TryUpdatePriority(edge.To, edgeDistance + AStarHeuristics(edge.To, Target));
-                }
-
-
-            }
-
-
-        }
-
-        private struct Edge
-        {
-            public int Value { get; }
-            public Coords To { get;}
-            public Edge(int value, Coords to)
-            {
-                Value = value;
-                To = to;
-            }
-        }
-
-        private List<Coords> GetNeighbourCoords(Coords coords)
-        {
-            List<Coords> neighbours = new List<Coords>();
-
-            if (coords.X + 1 < MaximumSize.X) neighbours.Add(new Coords(coords.X + 1, coords.Y, coords.Z));
-            if (coords.Y + 1 < MaximumSize.Y) neighbours.Add(new Coords(coords.X, coords.Y + 1, coords.Z));
-            if (coords.Y - 1 > 0) neighbours.Add(new Coords(coords.X, coords.Y - 1, coords.Z));
-            if (coords.X - 1 > 0) neighbours.Add(new Coords(coords.X - 1, coords.Y, coords.Z));
-
-            return neighbours;
-        }
-
-        private void PrintMap(int highlightX = -1, int highlightY = -1)
-        {
-            for (int y = 0; y < MaximumSize.Y; y++)
-            {
-                for (int x = 0; x < MaximumSize.X; x++)
-                {
-                    if (x == highlightX && y == highlightY) Console.BackgroundColor = ConsoleColor.Yellow;
-
-                    switch (RiskLevel[x, y])
-                    {
-                        case 0:
-                            Console.Write(".");
-                            break;
-                        case 1:
-                            Console.Write("=");
-                            break;
-                        case 2:
-                            Console.Write("|");
-                            break;
-                    }
-                    Console.ResetColor();
-                }
-                Console.WriteLine();
-            }
-            Console.WriteLine();
-        }
-
-        private int AStarHeuristics(Coords from, Coords to)
-        {
-            int guesstimate = 0;
-            if (from.Z != to.Z) guesstimate += 7;
-            guesstimate += Math.Abs(to.X - from.X);
-            guesstimate += Math.Abs(to.Y - from.Y);
-            return guesstimate;
+            long distance = 0;
+            distance += Math.Abs(first.X - second.X);
+            distance += Math.Abs(first.Y - second.Y);
+            distance += Math.Abs(first.Z - second.Z);
+            return distance;
         }
     }
 
