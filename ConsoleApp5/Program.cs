5de8b4ac9770b8da0fa310ce668a5f6ba13e223c
Vojtěch Srdečný <vojtech.srdecny@gmail.com>
2018-12-25 22:10:35 +0100

    24A

--- ConsoleApp5/Program.cs
+++ ConsoleApp5/Program.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using System.IO;
 using System.Diagnostics;
 using Priority_Queue;
+using System.Text.RegularExpressions;
 
 namespace ConsoleApp5
 {
@@ -15,86 +16,203 @@ namespace ConsoleApp5
         static void Main(string[] args)
         {
             string input = @"C:\Users\Vojta\Documents\input.txt";
-            Map map = new Map();
-            map.ParseInput(input);
-            map.FindStrongestNanobot();
+            Engine engine = new Engine();
+            engine.ParseInput(input);
+            engine.DoTheBattle();
 
             Console.WriteLine("Finished...");
             Console.ReadLine();
-
-            // 949 high
         }
 
     }
 
-    public struct Coords
+    class Group
     {
-        public long X { get; set; }
-        public long Y { get; set; }
-        public long Z { get; set; }
-        public Coords(long x, long y, long z)
-        {
-            X = x;
-            Y = y;
-            Z = z;
-        }
-    }  
+        public int Id { get; set; }
+        public enum Factions { Immune, Infection}
+        public enum Damage { bludgeoning = 1, cold = 2, radiation = 4, slashing = 8, fire = 16}
+        public int Units { get; set;}
+        public int Hitpoints { get; set; }
 
-    public struct Nanobot
-    {
-        public Coords Coordinates { get; }
-        public long Range { get; }
-        
-        public Nanobot(Coords coords, long range)
+        public Factions Faction { get; set; }
+        public int Initiative { get; set; }
+
+        public int AttackDamage { get; set; }
+        public Damage AttackType { get; set; }
+
+        public List<Damage> WeakAgainst { get; set; } = new List<Damage>();
+        public List<Damage> ImmuneAgainst { get; set; } = new List<Damage>();
+
+        public bool isDead { get { return Units <= 0; } }
+
+        public int TakesDamageFromGroup(Group group)
         {
-            Coordinates = coords;
-            Range = range;
+            if (ImmuneAgainst.Contains(group.AttackType)) return 0;
+            else if (WeakAgainst.Contains(group.AttackType)) return group.EffectiveDamage * 2;
+            else return group.EffectiveDamage;
+        }
+
+        public int EffectiveDamage { get { return AttackDamage * Units; } }
+
+        public override string ToString()
+        {
+            return $"{Enum.GetName(typeof(Factions), Faction)} Group {Id}";
         }
     }
 
-    class Map
+    public class Engine
     {
-        private List<Nanobot> Nanobots = new List<Nanobot>();
+        List<Group> Groups = new List<Group>();
         public void ParseInput(string input)
         {
-            StreamReader reader = new StreamReader(input);
-            var positionDelimiters = "pos=<,>".ToCharArray();
-            var rangeDelimiters = " r=".ToCharArray();
+            StreamReader file = new StreamReader(input);
+            string line = file.ReadLine();
+            int id = 1;
 
-            while (!reader.EndOfStream)
+            // Immune system
+            while (true)
             {
-                var words = reader.ReadLine().Split(',');
-                var positionX = words[0].Split(positionDelimiters, StringSplitOptions.RemoveEmptyEntries).Select(x => Int64.Parse(x)).ToArray();
-                var positionY = words[1].Split(positionDelimiters, StringSplitOptions.RemoveEmptyEntries).Select(x => Int64.Parse(x)).ToArray();
-                var positionZ = words[2].Split(positionDelimiters, StringSplitOptions.RemoveEmptyEntries).Select(x => Int64.Parse(x)).ToArray();
-                var range = words[3].Split(rangeDelimiters, StringSplitOptions.RemoveEmptyEntries).Select(x => Int64.Parse(x)).ToArray();
-                Nanobots.Add(new Nanobot(new Coords(positionX[0], positionY[0], positionZ[0]), range[0]));
+                line = file.ReadLine();
+                if (line == "") break;
+                ParseGroupFromLine(line);
+                var group = ParseGroupFromLine(line);
+                group.Faction = Group.Factions.Immune;
+                group.Id = id;
+                id++;
+                Groups.Add(group);
+            }
+            file.ReadLine();
+            id = 1;
+            while (!file.EndOfStream)
+            {
+                line = file.ReadLine();
+                var group = ParseGroupFromLine(line);
+                group.Faction = Group.Factions.Infection;
+                group.Id = id;
+                id++;
+                Groups.Add(group);
             }
         }
 
-        public void FindStrongestNanobot()
+        private Group ParseGroupFromLine(string line)
         {
-            int maxNanobotAmount = Int32.MinValue;
+            Group group = new Group();
+            var words = line.Split(' ');
 
-            int nanobotAmount = 0;
-            Nanobot strongestNanobot = Nanobots.OrderByDescending(x => x.Range).First();
-            foreach (var otherNanobot in Nanobots)
+            group.Initiative = Int32.Parse(words[words.Length - 1]);
+            group.Units = Int32.Parse(words[0]);
+            group.Hitpoints= Int32.Parse(words[4]);
+
+            Regex attackRegex = new Regex(@"(?<=does )\d+");
+            group.AttackDamage = Int32.Parse(attackRegex.Match(line).ToString());
+
+            for (int i = 0; i < words.Count() - 1; i++)
             {
-                if (CalculateManhattanDistance(strongestNanobot.Coordinates, otherNanobot.Coordinates) <= strongestNanobot.Range) nanobotAmount++;
+                if (words[i + 1] == "damage")
+                {
+                    Group.Damage type;
+                    Enum.TryParse(words[i], out type);
+                    group.AttackType = type;
+                    break;
+                }
             }
-            if (nanobotAmount > maxNanobotAmount) maxNanobotAmount = nanobotAmount;
-            Console.WriteLine(maxNanobotAmount);
+
+            Regex parenthesesContent = new Regex(@"\((.+)\)");
+            if (parenthesesContent.Match(line).Captures.Count > 0)
+            {
+                string parentheses = parenthesesContent.Match(line).ToString().Replace(")", String.Empty).Replace("(",String.Empty).Replace(",", String.Empty);
+
+                if (parentheses.Contains(";"))
+                {
+                    //immune
+                    foreach (var word in parentheses.Split(';')[0].Split(' '))
+                    {
+                        Group.Damage type;
+                        if (Enum.TryParse(word, out type))
+                        {
+                            group.ImmuneAgainst.Add(type);
+                        }
+                    }
+
+                    // weak
+                    foreach (var word in parentheses.Split(';')[1].Split(' '))
+                    {
+                        Group.Damage type;
+                        if (Enum.TryParse(word, out type))
+                        {
+                            group.WeakAgainst.Add(type);
+                        }
+                    }
+                }
+                else // only weak
+                {
+                    foreach (var word in parentheses.Split(' '))
+                    {
+                        Group.Damage type;
+                        if (Enum.TryParse(word, out type))
+                        {
+                            group.WeakAgainst.Add(type);
+                        }
+                    }
+                }
+
+            }
+
+            return group;
+
         }
 
-        private long CalculateManhattanDistance(Coords first, Coords second)
+        public void DoTheBattle()
         {
-            long distance = 0;
-            distance += Math.Abs(first.X - second.X);
-            distance += Math.Abs(first.Y - second.Y);
-            distance += Math.Abs(first.Z - second.Z);
-            return distance;
+
+            while (true)
+            {
+                List<Group> Infections = Groups.Where(x => x.Faction == Group.Factions.Infection && !x.isDead).ToList();
+                List<Group> Immunes = Groups.Where(x => x.Faction == Group.Factions.Immune && !x.isDead).ToList();
+
+                List<(Group, Group)> WhosAttackingWho = new List<(Group, Group)>();
+
+                foreach (var group in Groups.OrderByDescending(x => x.EffectiveDamage).ThenByDescending(x => x.Initiative))
+                {
+                    //Console.WriteLine($"Now choosing {group.ToString()}");
+                    if (group.Faction == Group.Factions.Immune && Infections.Any())
+                    {
+                        var target = Infections.OrderByDescending(x => x.TakesDamageFromGroup(group)).ThenByDescending(x => x.EffectiveDamage).ThenByDescending(x => x.Initiative).First();
+                        if (target.TakesDamageFromGroup(group) == 0) continue;
+                        WhosAttackingWho.Add((group, target));
+                        Infections.Remove(target);
+                    }
+                    else if (Immunes.Any())// Infections
+                    {
+                        var target = Immunes.OrderByDescending(x => x.TakesDamageFromGroup(group)).ThenByDescending(x => x.EffectiveDamage).ThenByDescending(x => x.Initiative).First();
+                        if (target.TakesDamageFromGroup(group) == 0) continue;
+                        WhosAttackingWho.Add((group, target));
+                        Immunes.Remove(target);
+                    }
+                }
+
+                foreach (var attackingDefending in WhosAttackingWho.OrderByDescending(x => x.Item1.Initiative))
+                {
+                    if (attackingDefending.Item1.isDead) continue;
+                    int previousUnits = attackingDefending.Item2.Units;
+                    int damageDealt = attackingDefending.Item2.TakesDamageFromGroup(attackingDefending.Item1);
+                    attackingDefending.Item2.Units -= damageDealt / attackingDefending.Item2.Hitpoints;
+
+                    //Console.WriteLine($"Unit {attackingDefending.Item1.ToString()} is dealing {damageDealt} to {attackingDefending.Item2.ToString()} killing {previousUnits - attackingDefending.Item2.Units}");
+
+                }
+
+                if (Groups.Where(x => x.Faction == Group.Factions.Immune).All(x => x.isDead) || Groups.Where(x => x.Faction == Group.Factions.Infection).All(x => x.isDead))
+                {
+                    Console.WriteLine($"Infections have left: {Groups.Where(x => x.Faction == Group.Factions.Infection).Sum(x => x.Units)}");
+                    Console.WriteLine($"Immunes have left: {Groups.Where(x => x.Faction == Group.Factions.Immune).Sum(x => x.Units)}");
+                    break;
+                }
+
+            }
         }
     }
+
 
 
 }
